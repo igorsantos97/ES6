c164516562160c5e0c20d43487d12d3b337bddc2
Igor Santos <igornzds@gmail.com>
2021-05-13 15:02:33 -0300

    finalizar aula e iniciar exercícios de variáveis

---
+++ 01-javascript-para-iniciantes/variaveis/script-aula.js
@@ -0,0 +1,36 @@
+var nome = 'André';
+var idade = 28;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade, nome);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Rafael',
+  cidade = 'Rio';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// Hosting
+console.log(comida);
+var comida = 'Pizza';
+console.log(comida);
+
+// var time = 'Vasco';
+// console.log(time);
+// time = 'Flamengo';
+// console.log(time);
+
+// let time = 'Vasco';
+// time = 'Flamengo';
+
+const time = 'Flamengo';
+
+console.log(time);
