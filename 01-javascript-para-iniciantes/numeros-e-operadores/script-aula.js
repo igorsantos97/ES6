dde5ad2900534e1c0c67bda5734c9a3d980c01e9
Igor Santos <igornzds@gmail.com>
2021-05-13 16:22:09 -0300

    finalizar aula e exercícios de números e operadores

---
+++ 01-javascript-para-iniciantes/numeros-e-operadores/script-aula.js
@@ -0,0 +1,20 @@
+var total1 = 10 + 5 + 10;
+var divisao = 200 / 5;
+var modulo = 433434 % 3;
+
+// console.log(divisao);
+
+var testeNaN = 'Isso é 100' / 2;
+// console.log(isNaN(testeNaN));
+
+var soma1 = 10 + 10 + 20 + (30 * 4) / 2 + 10;
+// console.log(soma1);
+
+var x = 5;
+console.log(++x); // 6
+
+// +<number> || -<number>
+var idade = '28';
+var somaIdade = 5;
+
+console.log(+idade + somaIdade);
---
+++ 01-javascript-para-iniciantes/numeros-e-operadores/script.js
@@ -0,0 +1,26 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + (5 * 2) / 2 + 20; // 35
+
+// Crie duas expressões que retornem NaN
+var exNaN1 = 'is' / 20;
+var exNaN2 = '10s' - 20;
+
+console.log(exNaN1, exNaN2);
+
+// Somar a string '200' com o número 50 e retornar 250
+var n1 = '200';
+var n2 = 50;
+var total = +n1 + n2;
+
+console.log(total);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var valor5 = 5;
+console.log(++valor5);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+
+console.log(peso);
