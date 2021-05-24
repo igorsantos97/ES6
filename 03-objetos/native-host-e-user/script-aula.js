49e127ac4005496db413a6674bff3184f42af2f6
Igor Santos <igornzds@gmail.com>
2021-05-24 15:35:58 -0300

    [OBJETOS] finalizar aula e exercícios de: native, host e user

---
+++ 03-objetos/native-host-e-user/script-aula.js
@@ -0,0 +1,7 @@
+console.log(typeof Array.dfsfdfds !== 'undefined');
+
+if (typeof Array.dfsfdfds !== 'undefined') {
+  console.log('Existe');
+} else {
+  console.log('Não existe');
+}
---
+++ 03-objetos/native-host-e-user/script.js
@@ -0,0 +1,22 @@
+// Liste 5 objetos nativos
+
+// Object
+// Function
+// Number
+// Boolean
+// String
+
+// Liste 5 objetos do browser
+
+// NodeList
+// HTMLLIElement
+// Document
+// HTMLAllCollection
+// window.history
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+// Webkits (que são implementações do chrome)
+// --- webkitIsFullScreen
+// --- webkitHidden
