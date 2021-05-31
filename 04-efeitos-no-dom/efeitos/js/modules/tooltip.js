fc748139e234f37d52a205b51410a47530a8d135
Igor Santos <igornzds@gmail.com>
2021-05-31 11:45:38 -0300

    [EFEITOS-DOM] finalizar tooltip

---
+++ 04-efeitos-no-dom/efeitos/js/modules/tooltip.js
@@ -0,0 +1,45 @@
+export default function initTooltip() {
+  const tooltips = document.querySelectorAll('[data-tooltip]');
+
+  const onMouseLeave = {
+    handleEvent() {
+      this.toolipBox.remove();
+      this.element.removeEventListener('mouseleave', onMouseLeave);
+      this.element.removeEventListener('mousemove', onMouseMove);
+    },
+  };
+
+  const onMouseMove = {
+    handleEvent(event) {
+      this.tooltipBox.style.top = event.pageY + 20 + 'px';
+      this.tooltipBox.style.left = event.pageX + 20 + 'px';
+    },
+  };
+
+  function onMouseOver() {
+    const tooltipBox = criarTooltipBox(this);
+
+    onMouseMove.tooltipBox = tooltipBox;
+    this.addEventListener('mousemove', onMouseMove);
+
+    onMouseLeave.toolipBox = tooltipBox;
+    onMouseLeave.element = this; // OBS: 'this' é o mapa
+    this.addEventListener('mouseleave', onMouseLeave);
+  }
+
+  function criarTooltipBox(element) {
+    const toolipBox = document.createElement('div');
+    const text = element.getAttribute('aria-label');
+    toolipBox.classList.add('tooltip');
+    toolipBox.innerText = text;
+
+    document.body.appendChild(toolipBox);
+    console.log(toolipBox);
+
+    return toolipBox;
+  }
+
+  tooltips.forEach((item) => {
+    item.addEventListener('mouseover', onMouseOver);
+  });
+}
