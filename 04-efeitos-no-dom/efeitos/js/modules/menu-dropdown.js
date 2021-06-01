604048a0a2416c0eeb0dee4ec75bc47dc539c914
Igor Santos <igornzds@gmail.com>
2021-06-01 17:18:46 -0300

    [EFEITOS-DOM] iniciar dropdown menu

---
+++ 04-efeitos-no-dom/efeitos/js/modules/menu-dropdown.js
@@ -0,0 +1,15 @@
+export default function initMenuDropdown() {}
+
+const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+
+dropdownMenus.forEach((menu) => {
+  ['touchstart', 'click'].forEach((userEvent) => {
+    menu.addEventListener(userEvent, handleClick);
+  });
+});
+
+function handleClick(event) {
+  event.preventDefault();
+
+  this.classList.toggle('active');
+}
